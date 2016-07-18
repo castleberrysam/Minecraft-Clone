985d31f49562f9bde098dd82ac4e3abf677ee8b7
Blackjak34 <castleberrysam@gmail.com>
2016-07-18 17:18:31 -0400

    Make unix scripts not working directory dependent

--- run-cygwin.sh
+++ run-cygwin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd "$(dirname "$(readlink -f "$0")")"
 source setldpath-cygwin.sh
 dist/win/game.exe
--- run-linux.sh
+++ run-linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd "$(dirname "$(readlink -f "$0")")"
 source setldpath-linux.sh
 dist/linux/game
